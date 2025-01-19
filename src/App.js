2ae0e50039eaa449cf9dab8e44535c14592b99f5
Balaji <20x51a0483@srecnandyal.edu.in>
2025-01-19 11:26:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import HomePage from "./pages/HomePage";
 import UserDetailPage from "./pages/UserDetailPage";
